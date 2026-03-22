4b2c72db7c27384f6bd9ee63fbeff8740de87424
Michal-Kilian <kilian.michal.kili@gmail.com>
2026-03-22 13:42:36 +0100

    switched to ultimate for built-in profiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2023.2")
-    type.set("IC") // Target IDE Platform
+    type.set("IU") // Target IDE Platform
 
     plugins.set(listOf("java", "org.jetbrains.kotlin"))
 }
